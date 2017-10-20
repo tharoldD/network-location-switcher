43bd6ffbe9e613b4177bd66cce69f57c52751298
harold <tharoldd@gmail.com>
2017-10-20 22:55:04 +0300

    Initial Commit

---
+++ Network Location Switcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Network Location Switcher
+//
+//  Created by Harold Turyasingura on 20/10/2017.
+//  Copyright © 2017 Harold Turyasingura. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Network Location Switcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Network Location Switcher
+//
+//  Created by Harold Turyasingura on 20/10/2017.
+//  Copyright © 2017 Harold Turyasingura. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
